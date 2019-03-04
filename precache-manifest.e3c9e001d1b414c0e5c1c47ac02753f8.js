27516b6f3aa134c74425337039133f6390601c6c
Talita Peneluppi <tablitarc@gmail.com>
2019-03-03 20:16:30 -0500

    Updates

---
+++ precache-manifest.e3c9e001d1b414c0e5c1c47ac02753f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdda1286eadb3924f5c6",
+    "url": "/talita-peneluppi-project5/static/js/runtime~main.cdda1286.js"
+  },
+  {
+    "revision": "27e4aea411fbdae41e54",
+    "url": "/talita-peneluppi-project5/static/js/main.27e4aea4.chunk.js"
+  },
+  {
+    "revision": "71248903b717893411bc",
+    "url": "/talita-peneluppi-project5/static/js/2.71248903.chunk.js"
+  },
+  {
+    "revision": "27e4aea411fbdae41e54",
+    "url": "/talita-peneluppi-project5/static/css/main.ed2d164c.chunk.css"
+  },
+  {
+    "revision": "7aeac70f451a9a6fe23efece0855855f",
+    "url": "/talita-peneluppi-project5/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/talita-peneluppi-project5/precache-manifest.e3c9e001d1b414c0e5c1c47ac02753f8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/talita-peneluppi-project5/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
